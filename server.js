b859b77ac93136cb421ac9fd72dcc1eade736679
WesamKhalil <wkhalil@tutamail.com>
2021-01-24 18:35:32 +0000

    Added routes folder for articles. Created a page called new and a partial form.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
 const app = express()
+const articleRouter = require('./routes/articles.js')
+
+const articles = [{
+    title: 'working',
+    date:  new Date(),
+    description: 'Hellow World'
+},{
+    title: 'working',
+    date:  new Date(),
+    description: 'Hellow World'
+}]
 
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: true}))
+
+app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {articles})
 })
 
 app.listen(process.env.PORT || 3000, () => {
